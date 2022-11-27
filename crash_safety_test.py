dc4092947e2a196c40b15502b87514758e4a56a8
Jialin Li <jlli@cs.washington.edu>
2022-11-26 21:48:05 -0800

    update crash safety test to python3

--- crash_safety_test.py
+++ crash_safety_test.py
@@ -9,30 +9,33 @@ test = "lab4test_c"
 output_file = "output.txt"
 
 def main():
-    garbage = open("garbage.txt", 'w');
+    garbage = open("garbage.txt", 'w')
     call(["make","clean"], stdout = garbage, stderr = garbage)
 
     if os.path.exists(output_file):
         os.remove(output_file)
-    print "clean finished."
+    print("clean finished.")
 
     call(["make"], stdout = garbage, stderr = garbage)
-    print "make finished."
+    print("make finished.")
 
     w = open(output_file, 'w')
 
-    print "Running " + test + " " + str(max_times) + " times. test output=" + output_file
+    print("Running " + test + " " + str(max_times) + " times. test output=" + output_file)
     process = Popen([r'make', 'qemu'], stdin=PIPE, stdout=w)
+    time.sleep(5)
     i = 1
     while process.poll() == None:
-        process.stdin.write("lab4test_c " + str(i) + "\n")
-        time.sleep(0.2)
-        print "finished i=" + str(i)
+        test_str = "lab4test_c " + str(i) + "\n"
+        process.stdin.write(test_str.encode())
+        process.stdin.flush()
+        time.sleep(1)
+        print("finished i=" + str(i))
         i += 1
         if i > max_times:
             break
     process.terminate()
-    print "killing qemu"
+    print("killing qemu")
     call(["pkill","qemu"], stdout = garbage, stderr = garbage)
     garbage.close()
     w.close()
@@ -41,11 +44,11 @@ def main():
     r = open(output_file, 'r')
     buf = r.read()
     if "Not consistent" in buf:
-        print "file system is not crash-safe"
+        print("file system is not crash-safe")
     elif "lab4test_c passed!" in buf:
-        print "file system is crash-safe"
+        print("file system is crash-safe")
     else:
-        print "test is not finished yet!"
+        print("test is not finished yet!")
 
     r.close()
 
