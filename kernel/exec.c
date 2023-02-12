2013b00a9ac3589df1b079320fddcdd29abd239a
Chanh Truong <chanht1@cs.washington.edu>
2023-02-11 20:25:32 -0800

    finisehd exec

--- kernel/exec.c
+++ kernel/exec.c
@@ -10,38 +10,48 @@
 
 int exec(char *path, char **argv) {
   // your code here
-  int i = 0;
+  int j = 0;
   while (true) {
-    if (!strncmp(args[i], '\0', 1)) {
-      i++;
+    if (strncmp(argv[j], '\0', 1) == 0) {
       break;
     }
-    i++;
+    j++;
   }
 
   struct vspace tmp;
   vspaceinit(&tmp);
 
   uint64_t rip;
-  vspaceloadcode(&tmp, path, rip);
+  vspaceloadcode(&tmp, path, &rip);
 
   vspaceinitstack(&tmp, SZ_2G);
 
-  char* args[n];
+  char* args[j];
   int total_size = 0;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < j; i++) {
     int len = strlen(argv[i]) + 1; // for null terminator
     total_size += len;
 
     vspacewritetova(&tmp, SZ_2G - total_size, argv[i], len);
-    args[i] = SZ_2G - total_size;
+    args[i] = (char*)(SZ_2G - total_size);
   }
 
-  while (total_size % 8 == 0) {
-    vspacewritetova(&tmp, SZ_2G - total_size - 1, "\0", 1);
-  }
-  
-  
+  int limit = SZ_2G - total_size - ((SZ_2G - total_size) % 8);
+
+  int start_of_ptr = limit - sizeof(char*) * j;
+  vspacewritetova(&tmp, start_of_ptr, args, sizeof(char*) * j);
+
+  struct proc *p = myproc();
+  //registers
+  p->tf->rip = rip;
+  p->tf->rsi = SZ_2G - total_size;
+  p->tf->rdi = j;
+  p->tf->rsp = SZ_2G;  // doesn't matter
+
+  vspacecopy(&myproc()->vspace, &tmp);
+  vspaceinstall(myproc());
+
+  vspacefree(&tmp);
 
   return -1;
 }
